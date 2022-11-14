ddf49c2c4d029f32449a921868569fcd1931cee9
Moha <mbastan@cs.stonybrook.edu>
2022-11-13 23:54:52 -0500

    most useful parameter search

--- seq2seq/decode.sh
+++ seq2seq/decode.sh
@@ -23,22 +23,27 @@ MODEL_RECOVER_PATH="${MODEL_DIR}"
 #   --bs 1 --beam_size 50 --length_penalty 0.8 --ngram_size 3 \
 #   --prune_factor 100 --sat_tolerance 1 --beta 0 --early_stop 1.5 \
 #   --save_path "${OUTPUT_DIR}/${OUTPUT_FILE}.${SPLIT}" --score_path "${OUTPUT_DIR}/${OUTPUT_FILE}.json"
-for BEAM in 10 20 30 40 50 60 70 80 90 100
+#70 80 90 100
+# 10 20 30 40 50 60 70 80 90 100
+for BEAM in 50
 do
-  for PRUNE in 10 20 30 40 50 60 70 80 90 100
+  for PRUNE in 50 60 70 80 90 100
   do
-    for LENGTHP in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9
+    for LENGTHP in 0.5 0.6 0.7 0.8 0.9
     do
-      for SAT in 0 1 2 3
+      for SAT in 1 2 3
       do
-        CUDA_VISIBLE_DEVICES=${DEVICES} python3 decode.py \
-          --model_name ${MODEL_RECOVER_PATH} \
-          --input_path ${INPUT} --reference_path ${TARGET} \
-          --constraint_file ${DATA_DIR}/constraint/${SPLIT}.constraint.json \
-          --min_tgt_length 8 --max_tgt_length 64 \
-          --bs 8 --beam_size $BEAM --length_penalty $LENGTHP --ngram_size 3 \
-          --prune_factor $PRUNE --sat_tolerance $SAT --beta 0 --early_stop 1.5 \
-          --save_path "${OUTPUT_DIR}/${OUTPUT_FILE}_${BEAM}_${PRUNE}_${LENGTHP}_${SAT}.${SPLIT}" --score_path "${OUTPUT_DIR}/${OUTPUT_FILE}_${BEAM}_${PRUNE}_${LENGTHP}_${SAT}.json"
+        OUT_FILE="${OUTPUT_DIR}/${OUTPUT_FILE}_${BEAM}_${PRUNE}_${LENGTHP}_${SAT}.${SPLIT}"
+        if [ ! -f $OUT_FILE ]; then
+          CUDA_VISIBLE_DEVICES=${DEVICES} python3 decode.py \
+            --model_name ${MODEL_RECOVER_PATH} \
+            --input_path ${INPUT} --reference_path ${TARGET} \
+            --constraint_file ${DATA_DIR}/constraint/${SPLIT}.constraint.json \
+            --min_tgt_length 8 --max_tgt_length 64 \
+            --bs 8 --beam_size $BEAM --length_penalty $LENGTHP --ngram_size 3 \
+            --prune_factor $PRUNE --sat_tolerance $SAT --beta 0 --early_stop 1.5 \
+            --save_path "${OUTPUT_DIR}/${OUTPUT_FILE}_${BEAM}_${PRUNE}_${LENGTHP}_${SAT}.${SPLIT}" --score_path "${OUTPUT_DIR}/${OUTPUT_FILE}_${BEAM}_${PRUNE}_${LENGTHP}_${SAT}.json"
+        fi
       done
     done
   done
